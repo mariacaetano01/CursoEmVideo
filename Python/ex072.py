fdd3ecf03bcffe5d6eb6624ffd29899d014da40e
Maria Fernanda Caetano <nanda2001caetano@gmail.com>
2022-09-29 21:55:09 -0300

    Alterações na pasta

---
+++ Python/ex072.py
@@ -0,0 +1,49 @@
+# Crie um programa que tenha uma dupla totalmente preenchida
+# com uma contagem por extenso, de zero até vinte. Seu programa
+# deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo
+# por extenso.
+
+números = (
+    'zero',
+    'um',
+    'dois',
+    'três',
+    'quatro',
+    'cinco',
+    'seis',
+    'sete',
+    'oito',
+    'nove',
+    'dez',
+    'onze',
+    'doze',
+    'treze',
+    'quatorze',
+    'quinze',
+    'deszesseis',
+    'dezessete',
+    'dezoito',
+    'dezenove',
+    'vinte'
+)
+
+print("Como se escreve meu número?")
+
+while True:
+    num = int(input('Digite um número de 0 a 20: '))
+
+    if 0 <= num <= 20:
+        print(f'Você digitou o número {números[num]}')
+        game = str(input("Quer jogar de novo? (escrever sem acento!) "))
+        game.lower()
+        if game == 'sim':
+            continue
+        if game == "nao":
+            print("Obrigado por usar este programa!")
+            break
+        else:
+            while game != "sim" or game != "nao":
+                gg = str(input("Essa resposta não é válida. Quer jogar de novo? "))
+
+    else:
+        print("Você digitou um número fora do intervalo!\n")
